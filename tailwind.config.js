c57090c68f11c9eb018cc4b4e9d427d46846066d
seema <86520288+Bhardwajseem@users.noreply.github.com>
2024-07-16 15:50:51 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        theme:'#f75023',
+        p:'#71717a',
+        h:'#374151'
+    },
+  },
+  plugins: [],
+}
+}
+
